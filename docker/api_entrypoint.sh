1b2ffd5d73a6d5cae0313e7d3db052bea1e7998e
Yeray Alvarez Romero <yeray.alvarez.romero@gmail.com>
2019-02-20 12:05:58 +0000

    fixing migrations with dask layout being loaded at the beginning

--- docker/api_entrypoint.sh
+++ docker/api_entrypoint.sh
@@ -7,10 +7,14 @@ set -e
 
 bash wait-for-it.sh --timeout=10 ${POSTGRES_HOST}:${POSTGRES_PORT}
 
-cd /src/api/weather_api
+
 
 if [[ "$@" == *--migrations* ]]; then
-    flask create_database_and_upgrade
+    cd /src/
+    python -c "from connectors.db_connection import create_db_if_not_exists;create_db_if_not_exists();"
+    python -c "from alembic_scripts.utils import alembic_upgrade_head;alembic_upgrade_head();"
+    python -c "from tasks.weather_etl import update_station_locations; update_station_locations.delay()"
 elif [[ "$@" == *--api* ]]; then
+    cd /src/api/weather_api
     uwsgi --socket 0.0.0.0:${API_PORT} --yaml /src/api/uwsgi.yaml
 fi
